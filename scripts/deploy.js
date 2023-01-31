f9eeb3850a9fc71a46cb7cf2f3f885f0fbbc736c
swapnaliss <swapnalisbhargude123@gmail.com>
2023-01-31 16:46:45 +0530

    contract deployed and saving deploy details

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,27 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
 async function main() {
-    const [deployer] = await ethers.getSigners();
-  
-    console.log("Deploying contracts with the account:", deployer.address);
-  
-    const Storage = await ethers.getContractFactory("Storage");
-    const storage = await Storage.deploy();
-  
-    console.log("Contracts address:", storage.address);
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+  const Storage = await hre.ethers.getContractFactory("Storage");
+  const storage = await Storage.deploy();
+
+  await storage.deployed();
+
+  console.log(`${storage.signer.address} deployed to ${storage.address}`);
+  let details = {
+    deployer: storage.signer.address,
+    contract: storage.address
+  };
+
+  fs.writeFile('details.json', JSON.stringify(details, null, 4), (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log("Details are saved")
+  })
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
