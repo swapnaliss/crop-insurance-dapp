842f1ffc5c7f879d4099f43a7210748a2db822e1
swapnaliss <swapnalisbhargude123@gmail.com>
2023-01-31 15:14:49 +0530

    contract deployment done

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      // See its defaults
+    }
+  },
   solidity: "0.8.17",
 };
