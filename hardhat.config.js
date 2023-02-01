0cc72eee25f9606940158f3231c19b73960fe25c
swapnaliss <swapnalisbhargude123@gmail.com>
2023-02-01 18:04:14 +0530

    connecting to goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "ganache",
+  defaultNetwork: "goerli",
   networks: {
     ganache: {
       url: "http://localhost:8545"
     },
+    goerli: {
+      url: `https://goerli.infura.io/v3/f3e43dec8ea4453a9dc00e08b5c3f9cb`,
+      accounts: [`0x` + process.env.PRIVATE_KEY],
+    },
   //   hardhat: {
   //     // See its defaults
   //   }
+
   },
   solidity: "0.8.17",
 };
