b27b120ea8a7e016ccbc4468ee408362bec816ce
swapnaliss <swapnalisbhargude123@gmail.com>
2023-02-02 10:38:47 +0530

    metamsk connected

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import '../styles/globals.css'
 import Navbar from './provider/Navbar'
+import { WalletProvider } from './provider/WalletProvider'
+import { MetaMaskProvider } from 'metamask-react'
 function MyApp({ Component, pageProps }) {
 
   return (
     <div>
-      <Navbar />
-      <Component {...pageProps} />
+      {/* <Navbar /> */}
+      {/* <Component {...pageProps} /> */}
+    <MetaMaskProvider>
+    <WalletProvider>
+          <Navbar />
+          <div className="relative min-h-screen">
+            <Component {...pageProps} />
+          </div>
+    </WalletProvider>
+    </MetaMaskProvider>
     </div>
   )
 }
