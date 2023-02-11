c028e54287316f8803a5aa9dcb3565f861f81ffd
swapnaliss <swapnalisbhargude123@gmail.com>
2023-02-11 17:34:15 +0530

    rendering polices on farmers components

--- pages/modules/Farmer.jsx
+++ pages/modules/Farmer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import { useInsurer } from '../hooks/useInsurer';
+import Card from '../components/Card';
 const Farmer = () => {
+  const { policiesFromMogno } = useInsurer();
+
   return (
-    <div>Farmer</div>
+    <div>Farmer
+
+      <div className="flex flex-wrap">
+        {policiesFromMogno && policiesFromMogno.map((policy, index) => {
+          return (
+            <div key={index} className="w-full md:w-1/4">
+              <Card
+                policyName={policy.policyName}
+                id={policy._id}
+                description={policy.description}
+                period={policy.period}
+                coveredAmount={policy.coveredAmount}
+                premium={policy.premium}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
