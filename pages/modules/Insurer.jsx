d9384738410c3d036e19573f03a14e18bb3ae217
swapnaliss <swapnalisbhargude123@gmail.com>
2023-02-12 14:04:02 +0530

    refactor

--- pages/modules/Insurer.jsx
+++ pages/modules/Insurer.jsx
@@ -1,49 +1,79 @@
+import React, { useState } from 'react';
 import CreatePolicy from '../components/CreatePolicy'
 import { useInsurer } from '../hooks/useInsurer';
 import Card from '../components/Card';
 import { useUserContext } from '../provider/UserProvider';
 
-const Insurer = ({policiesFromMogno}) => {
+const Insurer = ({ policiesFromMogno }) => {
   const { formVisible, handleToggleForm } = useInsurer();
+  const [activeTab, setActiveTab] = useState("all-policies");
   const { user } = useUserContext();
 
   return (
-    <div>
-      <button className="bg-white text-indigo-500 p-2 rounded-lg hover:bg-indigo-500 hover:text-white"
-        onClick={handleToggleForm}
-      >
-        Create Policy
-      </button>
-      {
-        formVisible
-          ?
-          (
-            <div className="container mx-auto py-5">
-              <CreatePolicy />
-            </div>
-          )
-          : null
-      }
-
-      <div className="flex flex-wrap">
-        {policiesFromMogno && policiesFromMogno.map((policy, index) => {
-          return (
-            <div key={index} className="w-full md:w-1/4">
-              <Card
-                policyName={policy.policyName}
-                id={policy._id}
-                description={policy.description}
-                period={policy.period}
-                coveredAmount={policy.coveredAmount}
-                premium={policy.premium}
-                isFarmer={user?.role === "insurer" ? false : true}
-                policy={policy}
-              />
-            </div>
-          )
-        })}
+    <main className='py-5'>
+      <div className="tabs ml-6">
+        <button
+          className={`tab-item py-2 px-4 rounded-md ${activeTab === "all-policies" ? "bg-indigo-500 text-white" : "bg-white text-indigo-500"}`}
+          onClick={() => setActiveTab("all-policies")}
+        >
+          All Policies
+        </button>
+        <button
+          className={`tab-item py-2 px-4 rounded-md ${activeTab === "application-recived" ? "bg-indigo-500 text-white" : "bg-white text-indigo-500"}`}
+          onClick={() => setActiveTab("application-recived")}
+        >
+          Application Recived
+        </button>
       </div>
-    </div>
+
+      {activeTab === "all-policies"
+        ? (
+          <div className="all-policies">
+            <div>
+              <button className="bg-white text-indigo-500 p-2 rounded-lg hover:bg-indigo-500 hover:text-white"
+                onClick={handleToggleForm}
+              >
+                Create Policy
+              </button>
+              {
+                formVisible
+                  ?
+                  (
+                    <div className="container mx-auto py-5">
+                      <CreatePolicy />
+                    </div>
+                  )
+                  : null
+              }
+            </div>
+
+            <div className="flex flex-wrap">
+              {policiesFromMogno && policiesFromMogno.map((policy, index) => {
+                return (
+                  <div key={index} className="w-full md:w-1/4">
+                    <Card
+                      policyName={policy.policyName}
+                      id={policy._id}
+                      description={policy.description}
+                      period={policy.period}
+                      coveredAmount={policy.coveredAmount}
+                      premium={policy.premium}
+                      isFarmer={user?.role === "insurer" ? false : true}
+                      policy={policy}
+                    />
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        ) :
+        (
+          <div className="application-recived">
+
+          </div>
+        )
+      }
+    </main >
   )
 }
 
